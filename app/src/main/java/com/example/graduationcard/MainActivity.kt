2e312718cbba422c456d34102d6ac269caed67ae
Katon Birawa <birawakaton@gmail.com>
2026-04-14 19:36:51 +0700

    Meambahkan kata doa dan mengganti warna background

--- app/src/main/java/com/example/graduationcard/MainActivity.kt
+++ app/src/main/java/com/example/graduationcard/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     GreetingText(
                         message = "Happy Graduation Katon",
-                        sender = "From Budi",
+                        sender = "From Birawa",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -39,15 +44,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, sender : String, modifier : Modifier = Modifier) {
-    Column (verticalArrangement = Arrangement.Center,
-    modifier = modifier.padding(8.dp).fillMaxSize()
-        ){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(8.dp)
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color(0xFF6A5ACD), Color(0xFFFF69B4)),
+                    start = Offset(0f, 0f),
+                    end = Offset(1000f, 1000f)
+                )
+            )
+
+    ){
 
         Text(
             text = message,
             fontSize = 74.sp,
             lineHeight = 110.sp,
             textAlign = TextAlign.Center
+
         )
 
 
@@ -56,12 +73,19 @@ fun GreetingText(message: String, sender : String, modifier : Modifier = Modifie
             fontSize = 28.sp,
             modifier = Modifier.padding(16.dp).align(Alignment.End)
         )
+
+        Text(
+            text = "Semoga apa yang katon cita-citakan menjadi Software Engineer dapat tercapai, Amin",
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 24.dp, start = 16.dp, end = 16.dp)
+        )
     }
 }
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     GraduationCardTheme {
-        GreetingText("Happy Graduation Katon", sender = "From Budi")
+        GreetingText("Happy Graduation Katon", sender = "From Birawa")
     }
 }
